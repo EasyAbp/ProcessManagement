84ef1075700898297d91a08175b994dc237b553f
gdlcf88 <gdlcf88@outlook.com>
2022-11-04 13:57:54 +0800

    Add `AreaAttribute` in `ProcessManagementController`

--- src/EasyAbp.ProcessManagement.HttpApi/EasyAbp/ProcessManagement/ProcessManagementController.cs
+++ src/EasyAbp.ProcessManagement.HttpApi/EasyAbp/ProcessManagement/ProcessManagementController.cs
@@ -1,8 +1,10 @@
 ﻿using EasyAbp.ProcessManagement.Localization;
+using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
 
 namespace EasyAbp.ProcessManagement;
 
+[Area(ProcessManagementRemoteServiceConsts.ModuleName)]
 public abstract class ProcessManagementController : AbpControllerBase
 {
     protected ProcessManagementController()
