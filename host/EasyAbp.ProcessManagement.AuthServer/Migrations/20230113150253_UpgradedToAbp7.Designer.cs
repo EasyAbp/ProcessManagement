e7c88ddbbb26a8bbb80598ff20e5e4bc1a3d0b71
gdlcf88 <gdlcf88@outlook.com>
2023-01-13 23:07:20 +0800

    Upgrade to ABP 7.0.0

--- host/EasyAbp.ProcessManagement.AuthServer/Migrations/20230113150253_UpgradedToAbp7.Designer.cs
+++ host/EasyAbp.ProcessManagement.AuthServer/Migrations/20230113150253_UpgradedToAbp7.Designer.cs
@@ -4,6 +4,7 @@ using EasyAbp.ProcessManagement.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace EasyAbp.ProcessManagement.Migrations
 {
     [DbContext(typeof(AuthServerDbContext))]
-    partial class AuthServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230113150253_UpgradedToAbp7")]
+    partial class UpgradedToAbp7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
